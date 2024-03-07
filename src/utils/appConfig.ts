7519c0c83b280aedf32d0b1ac4379734b150d962
zwn <1879154660@qq.com>
2024-03-07 16:56:38 +0800

    feat: pinia 持久化存储

--- src/utils/appConfig.ts
+++ src/utils/appConfig.ts
@@ -3,11 +3,16 @@ import SvgIcon from '@/components/SvgIcon/SvgIcon.vue';
 // import WujieVue from 'wujie-vue3';
 //
 import microApp from '@micro-zoe/micro-app';
-
+import { createPinia } from 'pinia';
+// pinia持久化存储
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 // 这个函数用于为vue实例添加功能的
 
 export const appConfig = (app: App<Element>) => {
 	app.component('SvgIcon', SvgIcon); // 全局注册SvgIcon
+	const pinia = createPinia()
+	pinia.use(piniaPluginPersistedstate)
+	app.use(pinia)
 	microApp.start(); // 初始化micro-app
 	// app.use(WujieVue);
 
