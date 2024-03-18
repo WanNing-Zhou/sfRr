a44f37ec1111919d30c86e31950eb8a6e789bc66
zwn <1879154660@qq.com>
2024-03-18 13:24:54 +0800

    feat: 添加标题组件

---
+++ src/components/WqTitle/WqTitle.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="wq-title">
+		<slot></slot>
+	</div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+	fontSize?: string;
+	lineHeight?: string;
+	marginBottom?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+	fontSize(props) {
+		return '18px';
+	},
+	lineHeight(props) {
+		return '20px';
+	},
+	marginBottom(props) {
+		return '20px';
+	},
+});
+</script>
+
+<style scoped>
+.wq-title {
+	//background: #1a1a1a;
+	text-align: center;
+	width: 100%;
+	//display: flex;
+	align-items: center;
+	font-size: v-bind(fontSize);
+	line-height: v-bind(lineHeight);
+	margin-bottom: v-bind(marginBottom);
+}
+</style>
